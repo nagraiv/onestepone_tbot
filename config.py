0fd43bd97537bca552c914636726545783021bcf
nagraiv <117268180+nagraiv@users.noreply.github.com>
2022-12-25 01:43:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-host = "156.67.76.8"
-login = "u692549505_telbot_admin"
-password = "p4@@9?:jE]Q"
-db_name = "u692549505_telbot"
+host = ""
+login = ""
+password = ""
+db_name = ""
 
-TOKEN = '5655237135:AAHnCh8UrlFAPrP6jhRd_sSZHJIf1MqGno8'
+TOKEN = ''
