3abb154fae340c178b27803c45bc08d932d94b5e
GraN <117268180+nagraiv@users.noreply.github.com>
2023-02-06 16:39:26 +0700

    Скорректированы запросы к БД.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-host = ""
-login = ""
-password = ""
-db_name = ""
+host = "156.67.76.8"
+login = "u692549505_telbot_admin"
+password = "p4@@9?:jE]Q"
+db_name = "u692549505_telbot"
 
-TOKEN = ''
+TOKEN = '1275740860:AAFWWQMw2PN1WUg4tSD7WU_5U-JQx1thaag'
